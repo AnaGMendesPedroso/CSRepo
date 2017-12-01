ca09d092cb8af9f915f0659551ecc58a3149cecd
Renato Gouvea <renatogouvea.se@gmail.com>
2017-12-01 11:08:11 -0300

    scan and get datas of the IEEE publisher finished

--- Src/development/CSRepo/src/main/java/com/facom/csrepo/wrappers/WrapperIEEE.java
+++ Src/development/CSRepo/src/main/java/com/facom/csrepo/wrappers/WrapperIEEE.java
@@ -20,45 +20,53 @@ import org.json.JSONObject;
  * @author Renato Gouvea
  */
 public class WrapperIEEE {
+    
+    public WrapperIEEE() {
+    }
 
     public static void main(String[] args) {
-        //String text1 = "http://ieeexploreapi.ieee.org/api/v1/search/articles?publication_title=";
-        //String text2 = "format=json&apikey=xsgdpynaaxuxtwummncmbbxs";
+        // method that returns papers from a searched conference
+        JSONArray papers = searchConference();
         
-        String text = getText("http://ieeexploreapi.ieee.org/api/v1/search/articles?publication_title=ICSE-SEET&max_records=30&format=json&apikey=xsgdpynaaxuxtwummncmbbxs");
-        //System.out.println("TEXTO:" + text);
-
-        //konvertierung in ein Json-Objekt
-        JSONObject resultSearch = new JSONObject(text);
-        String test = resultSearch.getString("articles");
-               
-        System.out.println("SUAHDSUAHUDAUSHD");
-        System.out.println(resultSearch);
-                
-        //JSONArray articles = resultSearch.getJSONArray("articles");
+        // method that builds metadata for each paper
+        buildMetadata(papers);
+    }    
         
-        //getMetadata(resultSearch, articles);
+        
+    public static JSONArray searchConference() {    
+        // search a conference in the IEE API and returns a string contents in Json format
+        String searchConference = getPapers("http://ieeexploreapi.ieee.org/api/v1/search/articles?publication_title=ICSE-SEET&max_records=5000&format=json&apikey=xsgdpynaaxuxtwummncmbbxs");
+        
+        // converting "getPapers" from String to Json object
+        JSONObject resultSearch = new JSONObject(searchConference);
+        
+        // getting the pappers through JSONArray
+        JSONArray papers = resultSearch.getJSONArray("articles");
+        //List<String> list = new ArrayList<String>();
+        
+        // total of papers returned from IEEE API
+        int totalPapers = resultSearch.getInt("total_records");
+        System.out.println(totalPapers);
+        
+        return papers;
     }
     
-    public static void getMetadata(JSONObject resultSearch, JSONArray articles){
-        //System.out.println("RESULTADO"+ text);
-        //text = json.toString();
-        int totalRecords = resultSearch.getInt("total_records");
-        //String title = articles.ge
-        System.out.println(totalRecords);
-        
+    public static void buildMetadata(JSONArray papers){
         //declarando atributos dos metadados de um paper
-        String blabla, paperTitle, nameConference, paperAuthor;
+        String paperTitle, nameConference, paperAuthor;
         int yearPublication;
-        //for (int i = 0; i < totalRecords; i++) {
+        
+        for (int i=0; i<papers.length(); i++) {
+            System.out.println("PAPER" + (i+1) + ": " + papers.getJSONObject(i).getString("title"));
+        }
+        //for (int i = 0; i < totalPapers; i++) {
             //resultSearch = articles.toJSONObject(articles);
                     //resultSearch.getString("title");
             //System.out.println(resultSearch);
         //}
-        
     }
     
-    public static String getText(String uriStr) {
+    public static String getPapers(String uriStr) {
         final StringBuffer buf = new StringBuffer(1000);    
         try {
             HTMLDocument doc = new HTMLDocument() {
