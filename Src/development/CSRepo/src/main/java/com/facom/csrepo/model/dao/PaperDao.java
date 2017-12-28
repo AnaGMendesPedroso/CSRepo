1e1fc4c1fe70f17cb9e2e2368115e7eee1c363d4
José Rafael <zerafael1@gmail.com>
2017-12-28 14:15:44 -0300

    Getting all conferences

--- Src/development/CSRepo/src/main/java/com/facom/csrepo/model/dao/PaperDao.java
+++ Src/development/CSRepo/src/main/java/com/facom/csrepo/model/dao/PaperDao.java
@@ -17,58 +17,44 @@ public class PaperDao extends GenericDao<Paper>{
 
     @Override
     public void insert(Paper paper) {
-        openCurrentSessionWithTransaction();
         getCurrentSession().save(paper);
-        closeCurrentSessionWithTransaction();
     }
     
     @Override
     public void delete(Paper paper) {
-        openCurrentSessionWithTransaction();
         getCurrentSession().delete(paper);
-        closeCurrentSessionWithTransaction();
     }
 
     @Override
     public void deleteById(Integer id) {
         Paper paper = findById(id);
-        openCurrentSessionWithTransaction();
         getCurrentSession().delete(paper);
-        closeCurrentSessionWithTransaction();
     }
     
     @Override
     public void update(Paper paper) {
-        openCurrentSessionWithTransaction();
         getCurrentSession().update(paper);
-        closeCurrentSessionWithTransaction();
     }
     
     @Override
     public List<Paper> findAll() {
-        openCurrentSession();
         List<Paper> papers = getCurrentSession().createQuery("FROM Paper").list();
-        closeCurrentSession();
         
         return papers;
     }
 
     @Override
     public Paper findById(Integer id) {
-        openCurrentSession();
         Query query = getCurrentSession().createQuery("FROM Paper WHERE id = :id");
         Paper paper = (Paper)query.setParameter("id", id).list().get(0);
-        closeCurrentSession();
         
         return paper;
     }
 
     @Override
     public List<Paper> findByName(String title) {
-        openCurrentSession();
         Query query = getCurrentSession().createQuery("FROM Paper WHERE title = :title");
         List<Paper> papers = query.setParameter("title", title).list();
-        closeCurrentSession();
         
         return papers;
     }
--- Src/development/CSRepo/src/main/java/com/facom/csrepo/wrappers/WrapperIEEE.java
+++ Src/development/CSRepo/src/main/java/com/facom/csrepo/wrappers/WrapperIEEE.java
@@ -1,9 +1,11 @@
 package com.facom.csrepo.wrappers;
 
 import com.facom.csrepo.model.Author;
+import com.facom.csrepo.model.Conference;
+import com.facom.csrepo.model.Edition;
 import com.facom.csrepo.model.Paper;
 import com.facom.csrepo.model.dao.AuthorDao;
-import com.facom.csrepo.model.dao.ConferenceDao;
+import com.facom.csrepo.model.dao.EditionDao;
 import com.facom.csrepo.model.dao.PaperDao;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,7 +14,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,43 +33,58 @@ import org.xml.sax.SAXException;
 public class WrapperIEEE {
     
     AuthorDao authorDao;
+    PaperDao paperDao;
+    EditionDao editionDao;
+    List<Conference> listConferences;
 
-    public WrapperIEEE(){
+//    public WrapperIEEE(){
+//        authorDao = new AuthorDao();
+//        authorDao.openCurrentSessionWithTransaction();
+//        paperDao = new PaperDao();
+//        start();
+//    }
+    
+    public WrapperIEEE(List<Conference> conferences){
         authorDao = new AuthorDao();
+        paperDao = new PaperDao();
+        editionDao = new EditionDao();
         authorDao.openCurrentSessionWithTransaction();
-        update();
+        paperDao.openCurrentSession();
+        editionDao.openCurrentSession();
+        start(conferences);
     }
     
-    public void update() {
-        Calendar scheduleUpdate = Calendar.getInstance();
-        scheduleUpdate.set(Calendar.MONTH, scheduleUpdate.get(Calendar.MONTH)+1);
-        scheduleUpdate.set(Calendar.DAY_OF_MONTH, 1);
-        scheduleUpdate.set(Calendar.HOUR, 0);
-        scheduleUpdate.set(Calendar.MINUTE, 0);
-        scheduleUpdate.set(Calendar.SECOND, 0);
-        System.out.println(scheduleUpdate.getTime());
-        Date data = scheduleUpdate.getTime();
-        //UpdateWrapper reminder = new UpdateWrapper(data);
+    public void start(List<Conference> conferences){
+        int year = Calendar.getInstance().get(Calendar.YEAR);
         
+        for(Conference conference : conferences){
+            // TODO: Verificar se a conferencia ja ocorreu esse ano
+            List<Edition> editions = editionDao.findByConferenceId(conference.getId());
+            // Verificar se a conferenceia deste ano ja esta adicionada
+            
             // method that returns papers from a searched conference
-        NodeList papers = searchConference();
+            NodeList papers = searchConference(conference, year);
+            // method that builds metadata for each paper
+            buildMetadata(papers);
+        }
+    }
 
-        // method that builds metadata for each paper
-        buildMetadata(papers);
-     }
-
-    public NodeList searchConference() {
-        String search = "http://ieeexploreapi.ieee.org/api/v1/search/articles?publication_title="
+    public NodeList searchConference(Conference conference, int year) {
+        String search;
+        String name, acronym, apiKey;
+        
+        search = "http://ieeexploreapi.ieee.org/api/v1/search/articles?publication_title="
                 + "NAMEANDACRONYM&publication_year=YEAR&max_records=1000&format=xml&apikey=APIKEY";
-        String name, acronym, year, apiKey;
-        name = "\"International Conference on Software Engineering\"";
-        acronym = "\"ICSE\"";
-        year = "2017";
+        name = "\"" + conference.getName() + "\"";
+//        name = "\"International Conference on Software Engineering\"";
+        acronym = "\"" + conference.getAcronym() + "\"";
+//        acronym = "\"ICSE\"";
+//        year = "2017";
         apiKey = "xsgdpynaaxuxtwummncmbbxs";
 
         search = search.replaceAll("NAME", name);
         search = search.replaceAll("ACRONYM", acronym);
-        search = search.replaceAll("YEAR", year);
+        search = search.replaceAll("YEAR", Integer.toString(year));
         search = search.replaceAll("APIKEY", apiKey);
         search = search.replaceAll(" ", "%20");
         System.out.println(search);
@@ -86,22 +102,29 @@ public class WrapperIEEE {
     public void buildMetadata(NodeList papers) {
         //declarando atributos dos metadados de um paper
         Paper paper;
-        String paperTitle, year;
+        String paperTitle;
         NodeList authors;
         List<Author> listAuthor;
+        
         int publicationYear, firstPage, lastPage, pages;
         int cont = 0;
 
+        // each article returned from API
         for (int i = 0; i < papers.getLength(); i++) {
             try {
-                // each article returned from API
                 Node node = papers.item(i);
-                //if (node.getNodeType() == Node.ELEMENT_NODE){
-
                 Element element = (Element) node;
+                
                 //get title
                 paperTitle = element.getElementsByTagName("title").item(0).getTextContent();
                 paperTitle = paperTitle.replaceAll("&#x0022;", "\"");
+                
+                //Check if the paper is already exist in the database
+                if(existPaper(paperTitle)){
+                    System.out.println("Paper already exist");
+                    continue;
+                }
+                
                 //get pages
                 firstPage = Integer.parseInt(element.getElementsByTagName("start_page").item(0).getTextContent());
                 lastPage = Integer.parseInt(element.getElementsByTagName("end_page").item(0).getTextContent());
@@ -109,15 +132,15 @@ public class WrapperIEEE {
                 //publication year
                 String publicationTitle = (element.getElementsByTagName("publication_title").item(0).getTextContent());
                 publicationYear = Integer.parseInt(publicationTitle.split(" ")[0]);
-                //insert data on DB
+
                 paper = new Paper(paperTitle, pages, publicationYear, firstPage, lastPage);
-                ConferenceDao conference = new ConferenceDao();
-//                //paper.setConference(conference.findById(200));
-//                insertPaperDB(paper);
+//                ConferenceDao conference = new ConferenceDao();
+//              paper.setConference(conference.findById(200));
 
                 //get authors
                 authors = element.getElementsByTagName("authors");
                 authors = ((Element) authors.item(0)).getElementsByTagName("author");
+                
                 listAuthor = new ArrayList<>();
                 Author author;
                 for (int j = 0; j < authors.getLength(); j++) {
@@ -127,37 +150,13 @@ public class WrapperIEEE {
                     System.out.println("Authors: " + listAuthor.get(j).getName());
                 }
                 insertAuthorDB(listAuthor, paper);
-
-            } catch (Exception e) {
-            }
+                
+            } catch (Exception e) {}
         }
+        paperDao.closeCurrentSession();
         authorDao.closeCurrentSessionWithTransaction();
     }
-
-    public void insertPaperDB(Paper paper) {
-        PaperDao paperDao = new PaperDao();
-        paperDao.insert(paper);
-    }
-
-    public void insertAuthorDB(List<Author> authors, Paper paper) {
-        Author author;
-        for (int i = 0; i < authors.size(); i++) {
-            List<Author> tmpAuthors = authorDao.findByName(authors.get(i).getName());
-            //boolean newAuthor = authorDao.findByName(authors.get(i).getName()).isEmpty();
-            
-            if(tmpAuthors.isEmpty()){
-                author = authors.get(i);
-                author.addPaper(paper);
-                authorDao.insert(author);
-            }else{
-                author = tmpAuthors.get(0);
-                author.addPaper(paper);
-                authorDao.update(author);
-//                authorDao.update(authors.get(i));
-            }
-        }
-    }
-
+    
     public Document getPapers(String uriStr) {
         try {
             URL url = new URL(uriStr);
@@ -183,5 +182,33 @@ public class WrapperIEEE {
             Logger.getLogger(WrapperIEEE.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
+    }
+
+    public void insertAuthorDB(List<Author> authors, Paper paper) {
+        Author author;
+        for (int i = 0; i < authors.size(); i++) {
+            List<Author> tmpAuthors = authorDao.findByName(authors.get(i).getName());
+            //boolean newAuthor = authorDao.findByName(authors.get(i).getName()).isEmpty();
+            
+            if(tmpAuthors.isEmpty()){
+                author = authors.get(i);
+                author.addPaper(paper);
+                authorDao.insert(author);
+            }else{
+                author = tmpAuthors.get(0);
+                author.addPaper(paper);
+                authorDao.update(author);
+//                authorDao.update(authors.get(i));
+            }
+        }
+    }
+    
+    private boolean existPaper(String paperTitle){
+        boolean exist;
+        
+        //Check if dao found a paper in database
+        exist = !paperDao.findByName(paperTitle).isEmpty();
+        
+        return exist;
     }
 }
